52dc83a18f118064118c13f727004fee6365a80d
Mint Collie <dragoncool57@yahoo.com>
2021-03-09 02:28:46 -0600

    switched to different style to fix button bug

--- TrivaApp/ViewController.swift
+++ TrivaApp/ViewController.swift
@@ -7,61 +7,6 @@
 
 import UIKit
 
-
-class ViewController: UIViewController {
-    @IBOutlet weak var questionText: UILabel!
-    @IBOutlet weak var trueButton: UIButton!
-    @IBOutlet weak var falseButton: UIButton!
-    
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        let url = "https://opentdb.com/api.php?amount=1&type=boolean"
-        getData(from: url)
-
-        
-         
-    }
-    
-    private func getData(from url: String){
-        let task = URLSession.shared.dataTask(with: URL(string: url)!, completionHandler: {data, response, error in
-            
-            guard let data = data, error == nil else {
-                print("something went wrong")
-                return
-            }
-            
-            //have data
-            
-            var result: Response?
-            do {
-                result = try JSONDecoder().decode(Response.self, from: data)
-            }
-            
-            catch {
-                print("failed to convert \(error.localizedDescription)")
-            }
-            
-            guard let json = result else {
-                return
-            }
-            print(json.response_code)
-            print(json.results[0].category)
-            print(json.results[0].type)
-            print(json.results[0].difficulty)
-            print(json.results[0].question)
-            
-            
-        })
-        task.resume()
-    }
-
-    
-}
-
-
 struct Response: Codable {
     let response_code: Int
     let results: Array<MyResults>
@@ -74,4 +19,56 @@ struct MyResults: Codable {
     let question: String
     let correct_answer: String
     let incorrect_answers: Array<String>
+}
+let urlString = "https://opentdb.com/api.php?amount=1&type=boolean"
+
+class ViewController: UIViewController {
+    @IBOutlet weak var questionText: UILabel!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    
+    let dispatchGroup = DispatchGroup()
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        getJson { (json) in
+            print("finished")
+        }
+        dispatchGroup.notify(queue: .main) {
+            print("all finished")
+        }
+//        if let url = URL(string: urlString)
+//            {
+//                URLSession.shared.dataTask(with: url) {data, res, err in
+//                    if let data = data {
+//                        print("hey")
+//
+//                        let decoder = JSONDecoder()
+//                        if let json = try? decoder.decode(Response.self, from: data){
+//                            print(json)
+//                        }
+//                    }
+//                }.resume()
+//                print("finished")
+//        }
+        
+    }
+    
+    func getJson(completion: @escaping (Response) -> ()) {
+        dispatchGroup.enter()
+        if  let url = URL(string: urlString)
+        {
+            URLSession.shared.dataTask(with: url) {data, res, err in
+                if let data = data {
+                    let decoder = JSONDecoder()
+                    if let json = try? decoder.decode(Response.self, from: data) {
+                        self.dispatchGroup.leave()
+                        completion(json)
+                    }
+                }
+            }.resume()
+        }
+    }
 }
